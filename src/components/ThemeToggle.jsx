d2d1d28cdcf42e371731a22a5ebc7b02d6aa21b0
binayak1320 <csebinayak.ku@gmail.com>
2025-02-14 10:56:55 +0600

    add cursor ponter in mode change button

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -5,7 +5,7 @@ const ThemeToggle = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <button onClick={toggleTheme} className="p-2">
+    <button onClick={toggleTheme} className="p-2 cursor-pointer">
       {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
     </button>
   );
